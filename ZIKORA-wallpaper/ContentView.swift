54d21431f5b2429797abe1ab0905e50f5ca652b7
oriental <oriental011212@gmail.com>
2026-08-06 15:11:48 +0800

    Initial Commit

---
+++ ZIKORA-wallpaper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZIKORA-wallpaper
+//
+//  Created by Oriental on 2026/8/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZIKORA-wallpaper/ZIKORA_wallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZIKORA_wallpaperApp.swift
+//  ZIKORA-wallpaper
+//
+//  Created by Oriental on 2026/8/6.
+//
+
+import SwiftUI
+
+@main
+struct ZIKORA_wallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
